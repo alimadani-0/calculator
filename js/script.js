37760c7abe19a20d38c8b2e61e81b0b821ead009
Ali Madani <54098734+alimadani-0@users.noreply.github.com>
2023-09-15 16:17:41 +0300

    Fix the display for entering dot
    - Prevent multiple dots being input
    - Append a zero before the dot if nothing has been input

--- js/script.js
+++ js/script.js
@@ -4,10 +4,24 @@ let primaryDisplay = document.querySelector('.primary-display');
 
 function updatePrimaryDisplay(event) {
     const input = event.currentTarget.textContent;
-    if (!(input === '0' && primaryDisplayValue ==='0')) {
-        primaryDisplayValue = primaryDisplayValue === '0' ? input : primaryDisplayValue + input;
-        primaryDisplay.textContent = primaryDisplayValue;
+    if (input === '.') {
+        if (primaryDisplayValue.includes(input)) {
+            primaryDisplayValue = primaryDisplayValue;
+        } else {
+            primaryDisplayValue = primaryDisplayValue === ''
+                ? '0' + input
+                : primaryDisplayValue + input;
+        };
+    } else if (input === '0') {
+        primaryDisplayValue = primaryDisplayValue === '0'
+            ? primaryDisplayValue
+            : primaryDisplayValue + input;
+    } else {
+        primaryDisplayValue = primaryDisplayValue === '0'
+            ? input
+            : primaryDisplayValue + input;
     };
+    primaryDisplay.textContent = primaryDisplayValue;
 }
 
 function numbersEventListeners() {
